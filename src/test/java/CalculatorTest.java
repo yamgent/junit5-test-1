a3c3f9ef37db2fbe5b3063217a2039e18020b41f
Tan Wang Leng <tanwangleng@outlook.com>
2018-02-09 21:28:33 +0800

    Rename Tester to CalculatorTest

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Tester {
+public class CalculatorTest {
 
     @Test
     public void JustATest() {
